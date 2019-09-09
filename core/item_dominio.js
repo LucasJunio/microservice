7bc37ae85a9532016b2f818535706c6f562c872b
fernanda marcilio <fernanda.marcilio@zallpy.com>
2019-09-09 14:58:35 -0300

    SAU-117 Ajusta rota tipo de parada

--- core/item_dominio.js
+++ core/item_dominio.js
@@ -2,8 +2,24 @@ const database = require("../utils/database.js");
 
 const baseQuery = `SELECT ds_item_dominio, cd_item_dominio FROM sau_item_dominio`;
 
-async function findTipoParada() {
-  let query = baseQuery + `\nWHERE cd_dominio = 'TIPO_PARADA' ORDER BY 1`;
+async function findTipoParada(context) {
+  let query = baseQuery + `\nWHERE cd_dominio = 'TIPO_PARADA'`;
+
+  if (
+    context.targetDate &&
+    context.refDate &&
+    context.annualDate &&
+    context.scheduledDate &&
+    context.urgentDeadline &&
+    context.years
+  ) {
+    //verificar o tipo
+    let tipo = "PA";
+    query += `\nAND CD_ITEM_DOMINIO = '${tipo}'`;
+    console.log(context);
+  }
+  query += `\nORDER BY 1`;
+
   const result = await database.simpleExecute(query);
   return result.rows;
 }
