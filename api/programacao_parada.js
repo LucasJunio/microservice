979768ca5e51841aefa7ec34b6378b552e09a9cc
fernanda marcilio <fernanda.marcilio@zallpy.com>
2019-09-11 09:42:33 -0300

    SAU-117 Aplica rota de atualizar reprogramação

--- api/programacao_parada.js
+++ api/programacao_parada.js
@@ -24,6 +24,18 @@ async function putCancelamento(req, res, next) {
 
 module.exports.putCancelamento = putCancelamento;
 
+async function putReprogramacao(req, res, next) {
+  try {
+    const rows = await parada_programada.updateReprogramação(req.body);
+
+    res.status(200).json(rows);
+  } catch (err) {
+    next(err);
+  }
+}
+
+module.exports.putReprogramacao = putReprogramacao;
+
 async function post(req, res, next) {
   try {
     const paradaProgramada = await parada_programada.create(req.body);
