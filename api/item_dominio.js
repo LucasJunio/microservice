4b7dabd905b453588440313f1100dee8e2d03f34
fernanda marcilio <fernanda.marcilio@zallpy.com>
2019-09-27 14:46:09 -0300

    SAU-123 e SAU-126 - Cria rotas de listagem de dominios

--- api/item_dominio.js
+++ api/item_dominio.js
@@ -56,3 +56,63 @@ async function getTags(req, res, next) {
 }
 
 module.exports.getTags = getTags;
+
+async function getPerfil(req, res, next) {
+  try {
+    const rows = await item_dominio.findPerfil();
+
+    res.status(200).json(rows);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+}
+
+module.exports.getPerfil = getPerfil;
+
+async function getTela(req, res, next) {
+  try {
+    const rows = await item_dominio.findTela();
+
+    res.status(200).json(rows);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+}
+
+module.exports.getTela = getTela;
+
+async function getUsuario(req, res, next) {
+  try {
+    const rows = await item_dominio.findUsuario();
+
+    res.status(200).json(rows);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+}
+
+module.exports.getUsuario = getUsuario;
+
+async function getDominio(req, res, next) {
+  try {
+    const rows = await item_dominio.findDominio();
+
+    res.status(200).json(rows);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+}
+
+module.exports.getDominio = getDominio;
+
+async function getItemDominio(req, res, next) {
+  try {
+    const rows = await item_dominio.findItemDominio();
+
+    res.status(200).json(rows);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+}
+
+module.exports.getItemDominio = getItemDominio;
