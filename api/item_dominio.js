92cd4a9912c8b0e1d49f368f630eb0ab14d32113
fernanda marcilio <fernanda.marcilio@zallpy.com>
2019-10-04 14:56:52 -0300

    SAU-37 Ajusta getTipoParada

--- api/item_dominio.js
+++ api/item_dominio.js
@@ -1,15 +1,26 @@
 const item_dominio = require("../core/item_dominio.js");
+const param_prog_paradas = require("../core/param_programacao_paradas.js");
+const moment = require("moment");
 
 async function getTipoParada(req, res, next) {
   try {
     const context = {};
 
-    context.targetDate = req.query.dt_inicio_programacao;
+    context.data_inicio_programacao = moment(
+      req.query.dt_inicio_programacao
+    ).format("DD/MM/YYYY");
+
+    const rowsProgParada = await param_prog_paradas.find(context);
+
+    context.annualDate = rowsProgParada[0]["DT_FINAL_PARADAS_ANUAIS"];
+    context.scheduledDate = rowsProgParada[0]["DT_FINAL_PARADAS_PROGRAMADA"];
+    context.urgentDeadline = rowsProgParada[0]["NR_PRAZO_PARADA_URGENTE"];
+
+    const rowsParadaLong = await param_prog_paradas.findNroAnosParadaLongoPrazo();
+
+    context.years = rowsParadaLong[0]["NR_ANOS_PARADA_LONGO_PRAZO"];
     context.refDate = req.query.dt_ref_mapa;
-    context.annualDate = req.query.dtFinalParadasAnuais;
-    context.scheduledDate = req.query.dtFinalParadasProgramada;
-    context.urgentDeadline = req.query.nrPrazoParadaUrgente;
-    context.years = req.query.anosParadaLongoPrazo;
+    context.targetDate = context.data_inicio_programacao;
 
     const rows = await item_dominio.findTipoParada(context);
 
