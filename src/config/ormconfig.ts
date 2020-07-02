2cf5004dc176e8ea1cb230f4255526ef7f456747
Flavio Gouveia <flaviorgouveia@gmail.com>
2020-07-02 16:08:25 -0300

    SAU-2100 Flavio Supero ajsute na conexão oracledb

--- src/config/ormconfig.ts
+++ src/config/ormconfig.ts
@@ -1,21 +1,33 @@
 import { createConnection, Connection } from 'typeorm'
+import { logger } from '../util/logger'
 const config = require('./config')()
 
 const migrations = []
 const entities = [`${config.path}/entities/*.${config.extension}`]
 
 export async function getDbConnection(): Promise<Connection> {
-  return createConnection({
-    name: config.name,
-    type: config.type,
-    host: config.host,
-    port: config.port,
-    username: config.username,
-    password: config.password,
-    sid: config.sid,
-    synchronize: false,
-    logging: config.logging,
-    entities,
-    migrations
-  })
+  try {
+    const conn = await createConnection({
+      name: config.name,
+      type: config.type,
+      host: config.host,
+      port: config.port,
+      username: config.username,
+      password: config.password,
+      sid: config.sid,
+      synchronize: false,
+      logging: config.logging,
+      entities,
+      migrations,
+      extra: {
+        connectionLimit: 20
+      }
+    })
+    logger.info('DB Connection: ' + conn.isConnected)
+
+    return conn
+  } catch (error) {
+    logger.error(error)
+    getDbConnection()
+  }
 }
