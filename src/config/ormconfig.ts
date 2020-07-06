d00c252b4d2fb28d0085af2086fbf65554ad0d85
Flavio Gouveia <flaviorgouveia@gmail.com>
2020-07-06 13:51:16 -0300

    SAU-2100 Flavio Supero ajustes do PP

--- src/config/ormconfig.ts
+++ src/config/ormconfig.ts
@@ -6,7 +6,7 @@ const migrations = []
 const entities = [`${config.path}/entities/*.${config.extension}`]
 
 export async function getDbConnection(): Promise<Connection> {
-  return createConnection({
+  const conn = await createConnection({
     name: config.name,
     type: config.type,
     host: config.host,
@@ -15,11 +15,16 @@ export async function getDbConnection(): Promise<Connection> {
     password: config.password,
     sid: config.sid,
     synchronize: false,
-    logging: config.logging,
+    logging: ['error', 'schema'],
+    maxQueryExecutionTime: 1000,
     entities,
     migrations,
     extra: {
       connectionLimit: 20
     }
   })
+
+  conn.driver.maxAliasLength = 100
+
+  return conn
 }
