6e8c9a7a673dcd4ec0752e071b7358b3d28a6a66
lucas.azevedo <lucas.verdine@gmail.com>
2022-02-16 19:06:52 -0300

    fix: :bug: SAUA-328 corrigir alteração de status de PP quando é aprovada reprogramação

--- src/modules/v1/reprogramacao/reprogramacaoParadaService.ts
+++ src/modules/v1/reprogramacao/reprogramacaoParadaService.ts
@@ -4,7 +4,7 @@ import { SauItemLookUpRepository } from '../../../repositories/sauItemLookupRepo
 import { SauHistProgramacaoParadaRepository } from '../../../repositories/sauHistProgramacaoParadaRepository'
 import { SauProgramacaoParadaRepository } from '../../../repositories/sauProgramacaoParadaRepository'
 import { ParadaProgramadaService } from '../parada_programada/paradaProgramadaService'
-
+import { parseISO } from 'date-fns'
 export interface IReprogramacaoParadaService {
   saveReprogramacaoParada(repro: any, authorization: string)
 }
@@ -28,6 +28,11 @@ export class ReprogramacaoParadaService implements IReprogramacaoParadaService {
     const parada = await this.sauProgramacaoParadaRepository.getById(repro.cdPp)
     const statusReprog = await this.sauItemLookUpRepository.getItemLookUpByCdAndId('AAPRV_USINA', 13)
 
+    parada.idTipoParada = await this.sauItemLookUpRepository.getTipoParadaByDate(
+      parseISO(parada.DT_CRIACAO_PARADA.toString()),
+      parseISO(parada.DT_HORA_INICIO_REPROGRAMACAO.toString())
+    )
+    
     const newParada = {
       ...parada,
       ID_STATUS_PROGRAMACAO: 'R',
